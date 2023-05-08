f7c459deb1e4e4a2a7b7429047a53ce0a2182520
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-05-08 20:00:29 -0300

    ...

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,36 +1,38 @@
-import styled from 'styled-components';
-import logo from './assets/logo.png';
-import perfil from './assets/perfil.png';
+import styled from "styled-components";
+import logo from "./assets/logo.png";
+import perfil from "./assets/perfil.png";
 
 const CabecalhoEstilizado = styled.header`
-    display:flex;
+    display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 2em 4em
-`
+    padding: 2em 4em;
+`;
 
 const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-around;
     flex-grow: .1;
-`
+`;
 
 const LinkEstilizado = styled.a`
- color: var(--azul-escuro);
- font-weight: 700;
-`
+    color: var(--azul-escuro);
+    font-weight: 700;
+`;
 
-function Cabecalho() {
+const Cabecalho = () => {
     return (
         <CabecalhoEstilizado>
-            <img src={logo} alt="logo da empresa Voll" />
+            <img src={logo} alt="logo da empresa voll" />
+
             <Container>
-                <img src={perfil} alt="imagem de perfil do usuário" />
+                <img src={perfil} alt="perfil" />
+
                 <LinkEstilizado href="#">Sair</LinkEstilizado>
             </Container>
         </CabecalhoEstilizado>
-    )
-}
+    );
+};
 
 export default Cabecalho;
--- src/components/Rodape/index.tsx
+++ src/components/Rodape/index.tsx
@@ -1,59 +1,65 @@
-import styled from 'styled-components';
-import whatsapp from './assets/whatsapp.png';
-import instagram from './assets/instagram.png';
-import google from './assets/google.png';
-import facebook from './assets/facebook.png';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import facebook from "./assets/facebook.png";
+import whatsapp from "./assets/whatsapp.png";
+import google from "./assets/google.png";
+import instagram from "./assets/instagram.png";
+import styled from "styled-components";
 
 const RodapeEstilizado = styled.footer`
-height: 100%;
-color: white;
-padding: 1em;
-background-color: var(--azul-escuro);
-text-align: center;
-`
-
+    height: 100%;
+    color: white;
+    padding: 1em;
+    background-color: var(--azul-escuro);
+    text-align: center;
+`;
 
 const ListaEstilizada = styled.ul`
-display: flex;
-justify-content: space-around;
-width: 10%;
-margin: 1em auto;
-`
-
+    display: flex;
+    justify-content: space-around;
+    width: 10%;
+    margin: 1em auto;
+`;
 
 const ItemEstilizado = styled.li`
-list-style-type: none;
-`
+    list-style-type: none;
+`;
 
+const Rodape = () => {
+    const listaDeImagens = [
+        {
+            src: facebook,
+            alt: "facebook",
+        },
+        {
+            src: whatsapp,
+            alt: "whatsapp",
+        },
+        {
+            src: google,
+            alt: "google",
+        },
+        {
+            src: instagram,
+            alt: "instagram",
+        },
+    ];
 
-function Rodape() {
     return (
         <RodapeEstilizado>
             <ListaEstilizada>
-                <ItemEstilizado>
-                    <a href="#">
-                        <img src={facebook} alt="logo do facebook" />
-                    </a>
-                </ItemEstilizado>
-                <ItemEstilizado>
-                    <a href="#">
-                        <img src={whatsapp} alt="logo do whatsapp" />
-                    </a>
-                </ItemEstilizado>
-                <ItemEstilizado>
-                    <a href="#">
-                        <img src={google} alt="logo do google" />
-                    </a>
-                </ItemEstilizado>
-                <ItemEstilizado>
-                    <a href="#">
-                        <img src={instagram} alt="logo do instagram" />
-                    </a>
-                </ItemEstilizado>
+                {listaDeImagens.map((img) => {
+                    return (
+                        <ItemEstilizado>
+                            <a href="#">
+                                <img src={img.src} alt={`Logo do ${img.alt}`} />
+                            </a>
+                        </ItemEstilizado>
+                    );
+                })}
             </ListaEstilizada>
             <p>2023 © Desenvolvido por Alura | Projeto fictício sem fins comerciais.</p>
         </RodapeEstilizado>
-    )
+    );
 }
 
 export default Rodape;
