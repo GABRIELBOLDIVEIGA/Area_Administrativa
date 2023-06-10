5965a35ff7c6463285cf9e76101081847214c764
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-06-10 13:51:54 -0300

    ...

--- src/useDadosProficional.ts
+++
@@ -1,8 +0,0 @@
-import IProficional from "./types/IProfissional";
-import useFetch from "./useFetch";
-
-const useDadosProficional = () => {
-    return useFetch<IProficional[]>({ url: "profissionais" });
-};
-
-export default useDadosProficional;
